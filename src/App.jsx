95e34c0ba631dac558a17c599934e7d1fa14b70d
Michael Irwin <mikesir87@gmail.com>
2024-03-18 23:20:28 -0400

    Add message

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
+import { message } from './Message'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React 17</h1>
+      <h3>{ message }</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
